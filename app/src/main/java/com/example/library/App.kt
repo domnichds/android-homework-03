dd3404754704195d7553c1610524768ea67227c4
Danil Domnich <domni4.d2@yandex.ru>
2025-03-26 00:24:48 +0300

    Добавить layout карточки и списка, добавить основные файлы

---
+++ app/src/main/java/com/example/library/App.kt
@@ -0,0 +1,7 @@
+package com.example.library
+
+import android.app.Application
+
+class App : Application() {
+
+}
---
+++ app/src/main/java/com/example/library/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.example.library
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+}
