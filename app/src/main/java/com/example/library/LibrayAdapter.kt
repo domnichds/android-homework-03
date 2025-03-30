71963ff4a3dd48d1abcf765fca0b6a20e9840ba4
Danil Domnich <domni4.d2@yandex.ru>
2025-03-31 01:35:20 +0300

    Добавить логику обработки нажатий

--- app/src/main/java/com/example/library/LibrayAdapter.kt
+++ app/src/main/java/com/example/library/LibrayAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.library
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,26 +8,41 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class LibraryAdapter(private val data: MutableList<LibraryItem>) :
-    RecyclerView.Adapter<LibraryAdapter.ViewHolder>() {
+interface  onItemClickListener {
+    fun onItemClick(item: LibraryItem)
+}
 
+// Объявление класса адаптера. Поле - список предметов библиотеки
+class LibraryAdapter(
+    private val data: MutableList<LibraryItem>,
+    private val itemClickListener: onItemClickListener
+) : RecyclerView.Adapter<LibraryAdapter.ViewHolder>() {
+    // Внутренний класс с сылками на элементы карточки
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val nameTextView: TextView = view.findViewById(R.id.nameTextView)
         val idTextView: TextView = view.findViewById(R.id.idTextView)
         val iconImageView: ImageView = view.findViewById(R.id.iconImageView)
     }
-
+    // Переопределение метода создания элемента списка
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // Подтягивание данных из макета и "надувание" их в объект ViewGroup
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_card, parent, false)
+        // Возврат созданного экземпляра ViewHolder
         return ViewHolder(view)
     }
-
+    // Переопределение метода отображения элемента на экране
     override fun onBindViewHolder(holder: ViewHolder, index: Int) {
+        // Подтягивание данных из списка и установка их в элементы карточки
         val item = data[index]
         holder.nameTextView.text = item.name
         holder.idTextView.text = item.id.toString()
         holder.iconImageView.setImageResource(R.drawable.book)
-    }
 
+        // Привязка слушателя к методу onItemClick
+        holder.itemView.setOnClickListener {
+            itemClickListener.onItemClick(item)
+        }
+    }
+    // Переопределение метода для получения количества элементов в списке
     override fun getItemCount(): Int = data.size
 }
--- app/src/main/java/com/example/library/MainActivity.kt
+++ app/src/main/java/com/example/library/MainActivity.kt
@@ -1,17 +1,19 @@
 package com.example.library
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), onItemClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Установка макета activity_main
         setContentView(R.layout.activity_main)
-
+        // Поиск элемента recyclerView
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
-
+        // Тестовые данные
         val libraryItems: MutableList<LibraryItem> = mutableListOf(
             Book(0, "Преступление и наказание", true, 300, "Достоевский"),
             Book(0, "Война и мир", true, 12000, "Толстой"),
@@ -27,9 +29,16 @@ class MainActivity : AppCompatActivity() {
             Book(0, "1984", true, 400, "Оруэл"),
             Book(0, "1984", true, 400, "Оруэл")
         )
-
-        val adapter = LibraryAdapter(libraryItems)
+        // Создание адаптера и передача ему списка объектов
+        val adapter = LibraryAdapter(libraryItems, this)
+        // Указание recyclerView правила располагания элементов (список в данном случае)
         recyclerView.layoutManager = LinearLayoutManager(this)
+        // Установка адаптера для recyclerView
         recyclerView.adapter = adapter
+    }
+
+    override fun onItemClick(item: LibraryItem) {
+        // Вывод короткого тоста с текстом
+        Toast.makeText(this, "Элемент с id ${item.id}", Toast.LENGTH_SHORT).show()
     }
 }
