a8342400f1e8e34950b4fd558fba6a3fbb9959ca
Danil Domnich <domni4.d2@yandex.ru>
2025-03-31 14:57:00 +0300

    Добавить логику смены доступа к предмету

--- app/src/main/java/com/example/library/LibrayAdapter.kt
+++ app/src/main/java/com/example/library/LibrayAdapter.kt
@@ -6,19 +6,21 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 
-interface  onItemClickListener {
+interface  OnItemClickListener {
     fun onItemClick(item: LibraryItem)
 }
 
 // Объявление класса адаптера. Поле - список предметов библиотеки
 class LibraryAdapter(
     private val data: MutableList<LibraryItem>,
-    private val itemClickListener: onItemClickListener
+    private val itemClickListener: OnItemClickListener
 ) : RecyclerView.Adapter<LibraryAdapter.ViewHolder>() {
     // Внутренний класс с сылками на элементы карточки
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val cardView: CardView = view.findViewById(R.id.cardView)
         val nameTextView: TextView = view.findViewById(R.id.nameTextView)
         val idTextView: TextView = view.findViewById(R.id.idTextView)
         val iconImageView: ImageView = view.findViewById(R.id.iconImageView)
@@ -35,11 +37,35 @@ class LibraryAdapter(
         // Подтягивание данных из списка и установка их в элементы карточки
         val item = data[index]
         holder.nameTextView.text = item.name
-        holder.idTextView.text = item.id.toString()
-        holder.iconImageView.setImageResource(R.drawable.book)
+        holder.idTextView.text = "ID: ${item.id.toString()}"
+        holder.iconImageView.setImageResource(
+            when (item) {
+                is Book -> R.drawable.book
+                is Disk -> R.drawable.disk
+                is Newspaper -> R.drawable.newspaper
+                else -> R.drawable.unknown
+            }
+        )
+
+        Log.d("LibraryAdapter", "onBindViewHolder: Привязываем элемент с id=${item.id}, name=${item.name}")
+
+        // Установка стиля в зависимости от доступности
+        if (item.accessible) {
+            holder.cardView.elevation = holder.itemView.context.resources.getDimension(R.dimen.enable_elevation)
+            holder.nameTextView.alpha = 1f
+            holder.idTextView.alpha = 1f
+            holder.iconImageView.alpha = 1f
+        } else {
+            holder.cardView.elevation = holder.itemView.context.resources.getDimension(R.dimen.disable_elevation)
+            holder.nameTextView.alpha = 0.3f
+            holder.idTextView.alpha = 0.3f
+            holder.iconImageView.alpha = 0.3f
+        }
 
         // Привязка слушателя к методу onItemClick
         holder.itemView.setOnClickListener {
+            item.accessible = !item.accessible
+            notifyItemChanged(index)
             itemClickListener.onItemClick(item)
         }
     }
--- app/src/main/java/com/example/library/MainActivity.kt
+++ app/src/main/java/com/example/library/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity(), onItemClickListener {
+class MainActivity : AppCompatActivity(), OnItemClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Установка макета activity_main
@@ -15,19 +15,38 @@ class MainActivity : AppCompatActivity(), onItemClickListener {
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
         // Тестовые данные
         val libraryItems: MutableList<LibraryItem> = mutableListOf(
-            Book(0, "Преступление и наказание", true, 300, "Достоевский"),
-            Book(0, "Война и мир", true, 12000, "Толстой"),
-            Book(0, "1984", true, 400, "Оруэл"),
-            Book(0, "1984", true, 400, "Оруэл"),
-            Book(0, "1984", true, 400, "Оруэл"),
-            Book(0, "1984", true, 400, "Оруэл"),
-            Book(0, "1984", true, 400, "Оруэл"),
-            Book(0, "1984", true, 400, "Оруэл"),
-            Book(0, "1984", true, 400, "Оруэл"),
-            Book(0, "1984", true, 400, "Оруэл"),
-            Book(0, "1984", true, 400, "Оруэл"),
-            Book(0, "1984", true, 400, "Оруэл"),
-            Book(0, "1984", true, 400, "Оруэл")
+            Book(1, "Преступление и наказание", true, 300, "Фёдор Достоевский"),
+            Book(2, "Война и мир", true, 1200, "Лев Толстой"),
+            Book(3, "1984", true, 400, "Джордж Оруэлл"),
+            Book(4, "Мастер и Маргарита", true, 500, "Михаил Булгаков"),
+            Book(5, "Гарри Поттер и философский камень", true, 350, "Джоан Роулинг"),
+            Book(6, "Алиса в стране чудес", true, 250, "Льюис Кэрролл"),
+            Book(7, "Идиот", true, 600, "Фёдор Достоевский"),
+            Book(8, "Отцы и дети", true, 320, "Иван Тургенев"),
+            Book(9, "Ромео и Джульетта", true, 250, "Уильям Шекспир"),
+            Book(10, "Анна Каренина", true, 864, "Лев Толстой"),
+
+            Newspaper(11, "Комсомольская правда", true, 2345, Month.JANUARY),
+            Newspaper(12, "Московский комсомолец", true, 1204, Month.FEBRUARY),
+            Newspaper(13, "Известия", true, 5678, Month.MARCH),
+            Newspaper(14, "Аргументы и факты", true, 9821, Month.APRIL),
+            Newspaper(15, "Российская газета", true, 3050, Month.MAY),
+            Newspaper(16, "Ведомости", true, 1678, Month.JUNE),
+            Newspaper(17, "The New York Times", true, 6789, Month.JULY),
+            Newspaper(18, "The Guardian", true, 2345, Month.AUGUST),
+            Newspaper(19, "Frankfurter Allgemeine", true, 4567, Month.SEPTEMBER),
+            Newspaper(20, "Le Monde", true, 7890, Month.OCTOBER),
+
+            Disk(21, "Музыка Бетховена", true, 0),
+            Disk(22, "Лунная соната", true, 0),
+            Disk(23, "Рок-хиты 80-х", true, 0),
+            Disk(24, "Лучшие треки 2000-х", true, 0),
+            Disk(25, "Интерстеллар", true, 1),
+            Disk(26, "Матрица", true, 1),
+            Disk(27, "Начало", true, 1),
+            Disk(28, "Властелин колец", true, 1),
+            Disk(29, "Гарри Поттер: философский камень", true, 1),
+            Disk(30, "Звёздные войны: Новая надежда", true, 1)
         )
         // Создание адаптера и передача ему списка объектов
         val adapter = LibraryAdapter(libraryItems, this)
